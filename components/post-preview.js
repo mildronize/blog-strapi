c2a82d63d746778394a987be7e60ab72a738a10c
Thada Wangthammang <mildronize@gmail.com>
2020-11-26 22:42:59 +0700

    update preview

--- components/post-preview.js
+++ components/post-preview.js
@@ -14,7 +14,10 @@ export default function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} url={coverImage.url} />
+        {
+          coverImage && coverImage.url && <CoverImage slug={slug} title={title} url={coverImage.url} />
+        }
+        
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
@@ -22,10 +25,16 @@ export default function PostPreview({
         </Link>
       </h3>
       <div className="text-lg mb-4">
-        <Date dateString={date} />
+        {
+          date &&   <Date dateString={date} />
+        }
+      
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      {
+        author &&  <Avatar name={author.name} picture={author.picture} />
+      }
+
     </div>
   )
 }
